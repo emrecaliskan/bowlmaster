b03cb148444c11e92719f4dd71c74bd8c0f4b9b1
Emre Caliskan <emre.caliskan@outlook.com>
2015-10-20 10:33:01 -0700

    Add code to check whether pin is standing or down.

---
+++ Assets/Pin.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour {
+
+	private float standingThreshold = 3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(isStanding());
+	}
+	
+	public bool isStanding(){
+		Vector3 rotationInEuler = transform.rotation.eulerAngles;
+		var xTilt = rotationInEuler.x > 180f ? Mathf.Abs(rotationInEuler.x - 360f) : rotationInEuler.x;
+		var zTilt = rotationInEuler.z > 180f ? Mathf.Abs(rotationInEuler.z - 360f) : rotationInEuler.z;
+		return (xTilt < standingThreshold) && (zTilt < standingThreshold);
+	}
+}
