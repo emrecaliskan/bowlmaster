d4f01e56eec62f7c3226c30bf0d50e65c3976d59
Emre Caliskan <emre.caliskan@outlook.com>
2015-10-20 00:09:42 -0700

    Add motion to ball and audio source.

---
+++ Assets/Ball.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public float launchSpeed = 500;
+
+	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
+		
+		Launch();
+	}
+	
+	public void Launch(){
+		rigidBody.velocity  = new Vector3(0, 0, launchSpeed);
+		audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
