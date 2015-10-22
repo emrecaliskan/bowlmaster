d1c7c2370744a85d0a1457393bfda2ce29f6b6b5
Emre Caliskan <emre.caliskan@outlook.com>
2015-10-21 20:40:19 -0700

    Add Unit Testing Framework.

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+[Category("Sample Tests")]
+public class ActionMasterTest {
+
+	[Test]
+	public void PassingTest(){
+		Assert.AreEqual(1, 1);
+	}
+
+}
